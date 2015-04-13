d99284bf44fbdc4caac106cd24937efc82ff75fb
ZekeSulastin <zekesulastin@gmail.com>
2015-04-12 20:05:15 -0400

    Added FixUTF8 function from ZeroBrane

--- .config/i3/i3bar.lua
+++ .config/i3/i3bar.lua
@@ -49,6 +49,26 @@ function json_wrap(full_text,color,sep,bw) -- Because typing out the whole JSON 
 	return '{"full_text":"'..full_text..'","color":"'..color..'","separator":'..sep..',"separator_block_width":'..bw..'}'
 end
 
+function fixUTF8(s, replacement) -- Thank you, ZeroBrane ...
+  local p, len, invalid = 1, #s, {}
+  while p <= len do
+    if     p == s:find("[%z\1-\127]", p) then p = p + 1
+    elseif p == s:find("[\194-\223][\128-\191]", p) then p = p + 2
+    elseif p == s:find(       "\224[\160-\191][\128-\191]", p)
+        or p == s:find("[\225-\236][\128-\191][\128-\191]", p)
+        or p == s:find(       "\237[\128-\159][\128-\191]", p)
+        or p == s:find("[\238-\239][\128-\191][\128-\191]", p) then p = p + 3
+    elseif p == s:find(       "\240[\144-\191][\128-\191][\128-\191]", p)
+        or p == s:find("[\241-\243][\128-\191][\128-\191][\128-\191]", p)
+        or p == s:find(       "\244[\128-\143][\128-\191][\128-\191]", p) then p = p + 4
+    else
+      s = s:sub(1, p-1)..replacement..s:sub(p+1)
+      table.insert(invalid, p)
+    end
+  end
+  return s, invalid
+end
+
 function conky_init()
 	lan_interface = os.getenv("LAN_INTERFACE")
 	wlan_interface = os.getenv("WLAN_INTERFACE")
@@ -63,10 +83,10 @@ function conky_mpd()
 	mpd_title	= conky_parse("${mpd_title}")
 	mpd_name	= conky_parse("${mpd_name}")
 
-	if mpd_artist ~= "" then
-		mpd_artist = mpd_artist.." - "
+	if mpd_name ~= "" then
+		mpd_artist = fixUTF8(mpd_name,"*")..": "
 	else
-		mpd_artist = mpd_name..": "
+		mpd_artist = fixUTF8(mpd_artist,"*").." - "
 	end
 
 	mpd_songinfo = string.gsub(mpd_artist..mpd_title,'\\','\\\\')
